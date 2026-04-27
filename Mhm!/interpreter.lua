a524b087327f254e2237c7a930f24d908c330c70
aadenboy <78753213+aadenboy@users.noreply.github.com>
2026-04-27 10:41:00 -0700

    Update interpreter.lua

--- Mhm!/interpreter.lua
+++ Mhm!/interpreter.lua
@@ -16,12 +16,42 @@ local function dump(thing, depth, seen)
   return any and build:sub(1, -2).."\n"..prefix:sub(1, -2).."}" or "{}"
 end
 
-local file = io.open(arg[1], "r")
+local gargs = {}
+local flags = {
+    dump = false,         -- dump the parsed program expanded to the console and exit
+    parserdump = false,   -- dump the parsed program as a lua table to the console and exit
+    maxdepth = math.huge, -- maximum depth to show
+    direct = false,       -- only show the currently selected branch
+    show = ".+",          -- regex of which branches to show (overrides maxdepth and direct)
+    showloose = ".+",     -- same as show but doesn't preserve entire branch
+}
+for i=1, #arg do
+    local a = arg[i]
+    if a:match("^%-%-[^=]+=%-?%d*%.?%d+") then
+        local name, value = a:match("^%-%-(.-)=(%-?%d*%.?%d+)")
+        if type(flags[name]) == "number" then flags[name] = tonumber(value) or flags[name] end
+    elseif a:match("^%-%-[^=]+=") then
+        local name, value = a:match("^%-%-(.-)=(.+)")
+        if type(flags[name]) == "string" then flags[name] = value end
+    elseif a:sub(1, 2) == "--" and #a > 2 and type(flags[a:sub(3)]) == "boolean" then
+        flags[a:sub(3)] = true
+    else
+        table.insert(gargs, a)
+    end
+end
+if flags.show == ".+" then flags.show = nil end
+if flags.show then
+  flags.maxdepth = math.huge
+  flags.direct = false
+end
+
+local file = io.open(gargs[1], "r")
 assert(file, "File not found")
 local code = file:read("*a")
 file:close()
-code = code:gsub("[^%^%(%)%[%]]", "")
+code = code:gsub("[^%^%(%)%[%]\n]", "")
 print(code)
+local line = 1
 function parse(s)
   local commands = {}
   local i = 1
@@ -29,18 +59,29 @@ function parse(s)
     local at = s:sub(i)
     if at:match("^%^*[%(%)]") then
       local nestings, command, next = at:match("^(%^*)([%(%)])()")
-      table.insert(commands, {type = command, nestings = #nestings})
+      table.insert(commands, {type = command, nestings = #nestings, line = line})
       i = i + next - 1
     elseif at:match("^%^*%b[]") then
       local nestings, inner, next = at:match("^(%^*)(%b[])()")
+      local preline = line
       local incmds = parse(inner:sub(2, -2))
-      table.insert(commands, {type = "[]", nestings = #nestings, inner = incmds})
+      table.insert(commands, {type = "[]", nestings = #nestings, inner = incmds, line = preline})
       i = i + next - 1
+    elseif at:sub(1, 1) == "\n" then
+      line = line + 1
+      i = i + 1
     else i = i + 1 end
   until i > #s
   return commands
 end
 local commands = parse(code)
+if flags.dump then
+  print(code)
+  os.exit()
+elseif flags.parserdump then
+  print(dump(commands))
+  os.exit()
+end
 
 local memory = {pointer = 0}
 
@@ -63,9 +104,17 @@ function debug(m, pre, indent)
     local value = m[i] and (m[i].unusable and "X" or m[i].pointer) or 0
     local pad = (" "):rep(maxn - #tostring(value))
     s = s..index.."["..pad..value.."] "
-    if m[i] and not m[i].unusable then afters = afters..debug(m[i], pre..index, indent.."\t").."\n" end
+    local matches = not flags.show
+    if flags.show then for match in flags.show:gmatch("[^,]+") do
+        if (pre..index):match("^"..match.."$") then matches = true; break end
+    end end
+    if m[i] and not m[i].unusable and matches
+    and (not flags.direct or i == m.pointer)
+    and (not flags.maxdepth or #pre < flags.maxdepth) then
+      afters = afters.."\n"..debug(m[i], pre..index, indent.."\t")
+    end
   end
-  s = s.."\n"..afters:sub(1, -2)
+  s = s..afters
   return s
 end
 
@@ -83,8 +132,15 @@ end
 local previouscommand = ""
 function run(command)
   print("\x1B[2J\x1B[H"..previouscommand)
-  previouscommand = ("^"):rep(command.nestings)..command.type
-  print(debug(memory, ""))
+  previouscommand = ("^"):rep(command.nestings)..command.type.." at line "..command.line
+  local debugged = debug(memory, "")
+  debugged = debugged:gsub("([ \t]+([a-zA-Z]+) [^\n]+\n?)", function(a, index)
+    for match in flags.showloose:gmatch("[^,]+") do
+      if index:match("^"..match.."$") then return a end
+    end
+    return ""
+  end)
+  print(debugged)
   io.read()
   if command.type == ")" then
     local m = traverse(command.nestings)
